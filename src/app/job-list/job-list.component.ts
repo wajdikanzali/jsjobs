b55815b35dd782402249557a61aaf69c288f9a24
Waj <wajdi kanzali>
2017-11-15 23:04:38 +0100

    Angular 4 structure with an exemple of reactive forms

---
+++ src/app/job-list/job-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { JobService } from '../services/job.service';
+
+@Component({
+  selector: 'cc-job-list',
+  templateUrl: './job-list.component.html',
+  styleUrls: ['./job-list.component.css']
+})
+export class JobListComponent implements OnInit {
+
+	jobs = [];
+  error = '';
+
+  constructor(private http:Http, private jobService: JobService) { }
+
+  ngOnInit() {
+  	
+    this.jobService.getJobs()
+          .subscribe(
+              data => {
+                this.jobs = data;
+                console.log(data);
+              },
+              error => {
+                console.error(error);
+                this.error = error;
+              }
+            );
+
+  }
+
+
+
+}
