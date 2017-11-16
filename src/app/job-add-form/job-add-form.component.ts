8acdae4098a03de726b1e864bd340b29bc576114
Waj <wajdi kanzali>
2017-11-16 23:15:57 +0100

    manage radio buttons

--- src/app/job-add-form/job-add-form.component.ts
+++ src/app/job-add-form/job-add-form.component.ts
@@ -16,7 +16,32 @@ export class JobAddFormComponent implements OnInit {
 		{id: 3, name: 'cdd', value: 'fixed-term'},
 		{id: 4, name: 'indépendant', value: 'freelance'},
 		{id: 5, name: 'intérim', value: 'temp'},
-	]
+	];
+
+	currencies = [
+    {id: 1, name: 'euros', value: 'EU', symbol: '€'},
+    {id: 2, name: 'livres sterling', value: 'POUNDS', symbol: '£'},
+    {id: 3, name: 'francs CFA', value: 'CFA', symbol: 'CFA'},
+    {id: 4, name: 'dollars canadien', value: 'CAD', symbol: '$'}
+    ];
+
+  statuses = [
+    {id: 1, name: 'cadre', value: 'executive'},
+    {id: 1, name: 'employé', value: 'employee'}
+  ];
+
+  experience = [
+    { id: 1, name: 'junior', value: 'junior'},
+    { id: 2, name: 'medior', value: 'medior'},
+    { id: 3, name: 'senior', value: 'senior'}
+  ];
+
+  areas = [
+    {id: 1, name: 'aucun déplacements', value: 'none'},
+    {id: 2, name: 'déplacements régionaux', value: 'region'},
+    {id: 3, name: 'déplacements nationaux', value: 'nation'},
+    {id: 4, name: 'déplacements internationaux', value: 'international'}
+  ];
 
   constructor(private formBuilder: FormBuilder) { }
 
@@ -30,8 +55,8 @@ export class JobAddFormComponent implements OnInit {
   		zipcode: 35,
   		description: '',
   		contract: '',
-  		salary: 0,
-  		currency: 'dinars',
+  		salary: null,
+  		currency: '',
   		startdate: new Date(),
   		experience: '',
   		status: '',
