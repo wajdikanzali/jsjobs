c7ff99faed0dcb69c19119620650a77270916d08
Waj <wajdi kanzali>
2017-11-19 22:13:41 +0100

    implement routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router'
+
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
 import { JobListComponent } from './job-list/job-list.component';
 import { JobService } from './services/job.service';
 import { JobAddFormComponent } from './job-add-form/job-add-form.component';
 import { DaysAgoPipe } from './pipes/days-ago.pipe';
+import { HomeComponent } from './home/home.component';
+import { JobDetailsComponent } from './job-details/job-details.component';
+
+const routes = [
+  {path: '', component: HomeComponent},
+  {path: 'jobs/:id', component: JobDetailsComponent},
+  {path: 'jobs', component: JobListComponent},
+]
 
 @NgModule({
   declarations: [
@@ -15,13 +25,16 @@ import { DaysAgoPipe } from './pipes/days-ago.pipe';
     SearchComponent,
     JobListComponent,
     JobAddFormComponent,
-    DaysAgoPipe
+    DaysAgoPipe,
+    HomeComponent,
+    JobDetailsComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [JobService],
   bootstrap: [AppComponent]
