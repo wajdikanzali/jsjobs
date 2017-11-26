4acffd9674b75eabdeece8a78a5baa58cf1b1ebc
Waj <wajdi kanzali>
2017-11-26 16:34:03 +0100

    menage POST

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,14 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 let data = require('./jobs');
-console.log(data);
+
+let initialJobs = data.jobs;
+let addedJobs = [];
+
+const getAllJobs = () => {
+	return [...addedJobs, ...initialJobs]
+};
+
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
@@ -16,14 +23,16 @@ const api = express.Router();
 
 api.get('/jobs', (req, res) => {
 
-	res.json(data.jobs);
+	//res.json(data.jobs);
+	res.json(getAllJobs());
 
 });
 
 
 api.post('/jobs', (req, res) =>{
 	const job = req.body;
-	console.log(job);
+	addedJobs = [job, ...addedJobs];
+	console.log('number of jobs', getAllJobs().length);
 	res.json(job);
 
 });
